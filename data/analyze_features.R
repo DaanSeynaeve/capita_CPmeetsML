df1db619c39c9c934bad95b74eabcc5103edad97
Tom <tom.decroos@hotmail.com>
2016-05-11 18:38:17 +0200

    sync

--- data/analyze_features.R
+++ data/analyze_features.R
@@ -1,14 +1,29 @@
-prices = read.table(file = 'prices2013.dat',header=T)
+prices = read.table(file = 'prices2013.dat',header=T)[,]
 
-features = data.frame(prices$HolidayFlag,prices$WeekOfYear,prices$DayOfWeek,prices$PeriodOfDay,prices$ForecastWindProduction,prices$SystemLoadEA,prices$SMPEA,prices$ORKTemperature,prices$ORKWindspeed,prices$CO2Intensity)
+extra18h = abs(((prices$PeriodOfDay-12)%%48)-24)
+#extra10h = abs(((prices$PeriodOfDay-4)%%48)-24)
+features = data.frame(extra18h,prices$HolidayFlag,prices$WeekOfYear,prices$DayOfWeek,prices$PeriodOfDay,prices$ForecastWindProduction,prices$SystemLoadEA,prices$SMPEA,prices$ORKTemperature,prices$ORKWindspeed,prices$CO2Intensity)
 y = prices$SMPEP2
 cor(features,y,use='complete.obs')
 
+new = data.frame(prices,extra18h)
+write.table()
 #y_prev = c(rep(0,24),head(prices$SMPEP2,-24))
 
 #daysums = colSums(matrix(y_prev, nrow=24))/24
 #y_sumdayprev = rep(daysums,each=24)
 
+pricesperperiod = rowSums(matrix(prices$SMPEP2,nrow=48))
+plot(1:48*.5,pricesperperiod)
+max(pricesperperiod)
+
+new_feature = rep(pricesperperiod,38016/48)
+
+plot(prices$PeriodOfDay,prices$SMPEP2)
+plot(prices$PeriodOfDay,prices$SMPEA)
+plot(prices$SMPEA,prices$SMPEP2)
+plot(prices$SMPEP2,new_feature)
+cor(prices$SMPEP2,new_feature,use='complete.obs')
 
 sum(prices$SMPEP2 > 300)
 
