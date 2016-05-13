a8060d82f80129c326614938ba82e50ea23badd4
Tom <tom.decroos@hotmail.com>
2016-05-13 12:16:54 +0200

    sync

--- data/analyze_features.R
+++ data/analyze_features.R
@@ -1,21 +1,23 @@
-prices = read.table(file = 'prices2013.dat',header=T)[,]
+prices = read.table(file = 'prices2013.dat',header=T)
 
-extra18h = abs(((prices$PeriodOfDay-12)%%48)-24)
+prices$PeriodsToPeak = abs(((prices$PeriodOfDay-12)%%48)-24)
 #extra10h = abs(((prices$PeriodOfDay-4)%%48)-24)
-features = data.frame(extra18h,prices$HolidayFlag,prices$WeekOfYear,prices$DayOfWeek,prices$PeriodOfDay,prices$ForecastWindProduction,prices$SystemLoadEA,prices$SMPEA,prices$ORKTemperature,prices$ORKWindspeed,prices$CO2Intensity)
+features = data.frame(prices$PeriodsToPeak,prices$HolidayFlag,prices$WeekOfYear,prices$DayOfWeek,prices$PeriodOfDay,prices$ForecastWindProduction,prices$SystemLoadEA,prices$SMPEA,prices$ORKTemperature,prices$ORKWindspeed,prices$CO2Intensity)
 y = prices$SMPEP2
 cor(features,y,use='complete.obs')
+cor(prices$PeriodsToPeak,prices$SMPEP2,use='complete.obs')
 
 new = data.frame(prices,extra18h)
-write.table()
+write.table(new,file="newprices2013.dat")
 #y_prev = c(rep(0,24),head(prices$SMPEP2,-24))
 
 #daysums = colSums(matrix(y_prev, nrow=24))/24
 #y_sumdayprev = rep(daysums,each=24)
 
-pricesperperiod = rowSums(matrix(prices$SMPEP2,nrow=48))
-plot(1:48*.5,pricesperperiod)
+pricesperperiod = rowSums(matrix(prices$SMPEP2,nrow=48))/(38016/48)
+plot(1:48*.5,pricesperperiod,xlab="Timeslot (hour)",ylab = "Average price")
 max(pricesperperiod)
+
 
 new_feature = rep(pricesperperiod,38016/48)
 
